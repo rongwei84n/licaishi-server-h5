133433e0abe3cd62616ac554493c885f8a1cd00b
HeavenZhanghr <240964503@qq.com>
2018-03-08 18:50:48 +0800

    产品详情基础布局

--- H5/src/main.js
+++ H5/src/main.js
@@ -5,11 +5,7 @@ import router from './router'
 import FastClick from 'FastClick'
 import VueLazyload from 'vue-lazyload'
 import store from './store'
-<<<<<<< HEAD
-import { Cell, Spinner, Popup, Header, Button } from "mint-ui"
-=======
-import { Cell, Spinner, Popup, Header, Button, Navbar, TabItem, TabContainer, TabContainerItem} from "mint-ui"
->>>>>>> deb1b8f743019afb11b7c084c6dba67419b66ecc
+import { Cell, Spinner, Popup, Header, Button, Navbar, TabItem, TabContainer, TabContainerItem } from "mint-ui"
 
 import 'common/stylus/index.styl'
 import 'common/css/reset.css'
@@ -22,31 +18,26 @@ import ajax from "api/ajax"
 Vue.component(Cell.name, Cell)
 Vue.component(Spinner.name, Spinner)
 Vue.component(Popup.name, Popup)
-<<<<<<< HEAD
-Vue.component(Header.name, Header)
-Vue.component(Button.name, Button)
-=======
 Vue.component(Header.name, Header);
 Vue.component(Button.name, Button);
 Vue.component(Navbar.name, Navbar);
 Vue.component(TabItem.name, TabItem);
 Vue.component(TabContainer.name, TabContainer);
 Vue.component(TabContainerItem.name, TabContainerItem);
->>>>>>> deb1b8f743019afb11b7c084c6dba67419b66ecc
 
 FastClick.attach(document.body)
 
 Vue.use(VueLazyload, {
-  // loading: require('common/image/wangzai.jpg')
+    // loading: require('common/image/wangzai.jpg')
 })
 // Vue.use(ajax)
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  store,
-  render: h => h(App)
+    el: '#app',
+    router,
+    store,
+    render: h => h(App)
 })
 
 Vue.prototype.$ajax = ajax
