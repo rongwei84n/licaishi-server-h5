266f92a7caf9a6b63b6711e253dd5318bbd3135b
黄荣卫 <rongwei.huang@phicomm.com>
2018-03-10 22:56:56 +0800

    增加订单 数据绑定

---
+++ H5/src/components/order/OrderListItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <mt-cell :title="dorderId" value="待打款"/>
+    <mt-cell :title="dProdName"></mt-cell>
+    <mt-cell :title="dOrderAmount"></mt-cell>
+    <mt-cell :title="dRebatePresent"></mt-cell>
+    <mt-cell :title="dRebateAmount"></mt-cell>
+    <mt-cell :title="dPayStatus"></mt-cell>
+    <mt-cell :title="dCustomerName"></mt-cell>
+    <mt-cell :title="dCustomerPhone"></mt-cell>
+    <mt-cell value="取消订单"></mt-cell>
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "order-list-item",
+      props: {
+        orderId: "", //订单
+        prodName: "", //产品名称
+        orderAmount: "", //订单金额
+        rebatePresent: "", //返佣比例
+        rebateAmount: "",  //返佣金额
+        payStatus: "", //支付状态(待打款可以取消订单)
+        customerName: "", //客户姓名
+        customerPhone: "" //客户电话
+      },
+      data () {
+        return {
+          dorderId: "订单号: " + this.orderId,
+          dProdName: "产品名称: " + this.prodName,
+          dOrderAmount: "订单金额: " + this.orderAmount,
+          dRebatePresent: "返佣比例: " + this.rebatePresent,
+          dRebateAmount: "返佣金额: " + this.rebateAmount,
+          dPayStatus: "支付状态: " + this.payStatus,
+          dCustomerName: "客户姓名: " + this.customerName,
+          dCustomerPhone: "客户手机号: " + this.customerPhone,
+        }
+      },
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ H5/src/contanier/search/OrderListItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>order list item</div>
+</template>
+
+<script>
+    export default {
+        name: "order-list-item"
+    }
+</script>
+
+<style scoped>
+
+</style>
