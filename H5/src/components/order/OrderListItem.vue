ff997fc0a19fe8e4fd7af2a7f1a02850aa5cb736
黄荣卫 <rongwei.huang@phicomm.com>
2018-03-22 19:41:09 +0800

    修改订单取消会跳转到订单详情的bug

--- H5/src/components/order/OrderListItem.vue
+++ H5/src/components/order/OrderListItem.vue
@@ -1,23 +1,25 @@
 <template>
-  <div class="all-content" v-on:click="gotoDetail">
-    <mt-cell :title="cDisplayOrderId">
-      <div class="order_status_waitpay" v-if="status==='01'">{{cDisplayStatus}}</div>
-      <div class="order_status_waitrebate" v-if="status==='02'">{{cDisplayStatus}}</div>
-      <div class="order_status_already_rebate" v-if="status==='03'">{{cDisplayStatus}}</div>
-      <div class="order_status_already_failed" v-if="status==='99'">{{cDisplayStatus}}</div>
-    </mt-cell>
-    <mt-cell :title="cDisplayProdName"></mt-cell>
-    <mt-cell>
-      <div slot="title" class="title_prodname">{{cDisplayOrderAmount}}</div>
-    </mt-cell>
-    <mt-cell>
-      <div slot="title" class="title_prodname">{{cDisplayRebatePresent}}</div>
-    </mt-cell>
-    <mt-cell>
-      <div slot="title" class="title_prodname">{{cDisplayRebateAmount}}</div>
-    </mt-cell>
-    <mt-cell :title="cDisplayVoucherStatus"></mt-cell>
-    <mt-cell :title="cDisplayCustomerName"></mt-cell>
+  <div class="all-content">
+    <div v-on:click="gotoDetail">
+      <mt-cell :title="cDisplayOrderId">
+        <div class="order_status_waitpay" v-if="status==='01'">{{cDisplayStatus}}</div>
+        <div class="order_status_waitrebate" v-if="status==='02'">{{cDisplayStatus}}</div>
+        <div class="order_status_already_rebate" v-if="status==='03'">{{cDisplayStatus}}</div>
+        <div class="order_status_already_failed" v-if="status==='99'">{{cDisplayStatus}}</div>
+      </mt-cell>
+      <mt-cell :title="cDisplayProdName"></mt-cell>
+      <mt-cell>
+        <div slot="title" class="title_prodname">{{cDisplayOrderAmount}}</div>
+      </mt-cell>
+      <mt-cell>
+        <div slot="title" class="title_prodname">{{cDisplayRebatePresent}}</div>
+      </mt-cell>
+      <mt-cell>
+        <div slot="title" class="title_prodname">{{cDisplayRebateAmount}}</div>
+      </mt-cell>
+      <mt-cell :title="cDisplayVoucherStatus"></mt-cell>
+      <mt-cell :title="cDisplayCustomerName"></mt-cell>
+    </div>
     <mt-cell v-if="status==='01'">
       <div class="item_cancel_order" v-on:click="cancelOrder">取消订单</div>
     </mt-cell>
@@ -43,42 +45,42 @@
     data() {
       return {}
     },
-    computed:{
-      cDisplayOrderId(){
+    computed: {
+      cDisplayOrderId() {
         return "订单号: " + this.orderId;
       },
-      cDisplayProdName(){
+      cDisplayProdName() {
         return "产品名称: " + this.prodName;
       },
-      cDisplayOrderAmount(){
+      cDisplayOrderAmount() {
         return "订单金额: " + this.orderAmount + "元";
       },
-      cDisplayRebatePresent(){
+      cDisplayRebatePresent() {
         return "返佣比例: " + this.rebatePresent;
       },
-      cDisplayRebateAmount(){
+      cDisplayRebateAmount() {
         return "返佣金额: " + this.rebateAmount + "元";
       },
-      cDisplayVoucherStatus(){ //支付状态
-        if(this.voucharStatus === "1") {
+      cDisplayVoucherStatus() { //支付状态
+        if (this.voucharStatus === "1") {
           return "支付状态: 已支付";
-        }else {
+        } else {
           return "支付状态: 未支付";
         }
       },
-      cDisplayStatus(){ //订单状态
-        if(this.status === "01") {
+      cDisplayStatus() { //订单状态
+        if (this.status === "01") {
           return "待打款";
-        }else if(this.status === "02") {
+        } else if (this.status === "02") {
           return "待结佣";
-        }else if(this.status === "03") {
+        } else if (this.status === "03") {
           return "已结佣";
-        }else if(this.status === "99") {
+        } else if (this.status === "99") {
           return "已失效";
         }
         return this.status;
       },
-      cDisplayCustomerName(){
+      cDisplayCustomerName() {
         return "客户姓名: " + this.customerName;
       },
     },
@@ -89,7 +91,7 @@
           url: `/srv/v1/order/cancelOrder?orderNo=${this.orderId}`,
           method: "POST"
         }).then(res => {
-          if(res.status === 200) {
+          if (res.status === 200) {
             console.debug("cancel 成功");
           }
         });
@@ -128,7 +130,7 @@
     }
     .item_cancel_order {
       color: #212121;
-      font-size:14px;
+      font-size: 14px;
     }
   }
 
